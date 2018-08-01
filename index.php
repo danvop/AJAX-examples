e137c4022cc0d66d3827f6e3b57e3737499e1497
Попов Данила Александрович <danvop@gmail.com>
2018-08-01 16:52:34 +1000

    get array hint

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html>
 <head>
     <title>Ajax test</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
+<div class="menu">
 <a href="getTextHint.html" target="blank">Get Text Hint</a>
+<a href="getArrayHint.html" target="blank">Get Array Hint</a>   
+</div>
+
 <button onclick="openAjax()">Open Request</button>
 <button onclick="sendAjax()">Send Request</button>
 </body>
